9d3d48ead1856c794eb3aee94257ee764822db8c
TiegoRama <ramatiego@gmail.com>
2024-09-10 08:32:25 +0200

    exo fait

--- 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    // put your code here
+    val a = 6
+    var res =""
+    if (a % 2 == 0){
+        res = "Divisible par 2"
+    }
+    if (a % 3 == 0){
+        res += " Divisible par 3"
+}
+    if (a % 5 == 0){
+        res += " Divisible par 5"
+    }
+    if (a % 6 == 0){
+        res += " Divisible par 6"
+    }
+    println(res)
 }

22fa6ca20543ebd635d25dceeac99ade1dbf14dd
TiegoRama <ramatiego@gmail.com>
2024-09-17 11:07:39 +0200

    exo fait

--- 17-Programmation_procedurale/17_4-Arguments_nommes/P17_4_a-Perimetre/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_4-Arguments_nommes/P17_4_a-Perimetre/src/main/kotlin/main.kt
@@ -1,7 +1,12 @@
-fun perimeter(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double, y4: Double): Double {
-    return 0.0
+import kotlin.math.pow
+import kotlin.math.hypot
+fun perimeter(x1: Double = 0.0 , y1: Double= 0.0, x2: Double= 0.0, y2: Double= 0.0, x3: Double= 0.0, y3: Double= 0.0, x4: Double= 0.0, y4: Double= 0.0): Double {
+    val xy = Math.hypot((x2-x1),(y2-y1)) + Math.hypot((x4-x3),(y4-y3)) + Math.hypot((x3-x2),(y3-y2))
+    return xy
 }
 
 fun main() {
     println(perimeter(0.0, 0.0, 12.0, 0.0, 0.0, 5.0))
 }
+
+/*(x.pow(2)+y.pow(2)).pow(0.5)*/
